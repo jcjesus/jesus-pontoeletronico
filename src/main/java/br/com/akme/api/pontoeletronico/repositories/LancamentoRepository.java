6917831f02e0915ac6b4d90f7434a926e8861dd1
Julio Jesus <mainjesus@gmail.com>
2018-06-24 22:06:31 -0300

    Cap13 - parte 1 - Criado camada service para empresa.

--- src/main/java/br/com/akme/api/pontoeletronico/repositories/LancamentoRepository.java
+++ src/main/java/br/com/akme/api/pontoeletronico/repositories/LancamentoRepository.java
@@ -1,7 +1,9 @@
 package br.com.akme.api.pontoeletronico.repositories;
 
 import br.com.akme.api.pontoeletronico.entities.Lancamento;
+
 import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
--- src/test/java/br/com/akme/api/pontoeletronico/repositories/LancamentoRepositoryTest.java
+++ src/test/java/br/com/akme/api/pontoeletronico/repositories/LancamentoRepositoryTest.java
@@ -12,10 +12,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.data.domain.PageRequest;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.util.Date;
 import java.util.List;
